9d4eae78d8470b832b861d06b7b0c6d4152ebed6
Pontus Stenetorp <pontus@stenetorp.se>
2013-01-12 00:55:05 +0900

    Attach the version to the archives

--- pack.sh
+++ pack.sh
@@ -5,9 +5,10 @@
 # Author:   Pontus Stenetorp    <pontus stenetorp se>
 # Version:  2013-01-11
 
+VERSION=1
 SCRIPT_DIR=`dirname ${0}`
 
 for dir in `find ${SCRIPT_DIR}/* -maxdepth 1 -type d`
 do
-    tar -c -z -C ${SCRIPT_DIR} -f ${dir}.tar.gz ${dir}
+    tar -c -z -C ${SCRIPT_DIR} -f ${dir}_v${VERSION}.tar.gz ${dir}
 done
